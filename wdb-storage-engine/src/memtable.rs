7b8a3d39fa31eaaee0acb4fb97c4de8cef51c311
Aleksander Piluk <aleksander.piluk@lensthat.co>
2024-05-26 01:22:07 +0000

    mvcc read

--- wdb-storage-engine/src/memtable.rs
+++ wdb-storage-engine/src/memtable.rs
@@ -1,6 +1,6 @@
-use crossbeam_skiplist::SkipSet;
+use crossbeam_skiplist::{set::Entry, SkipSet};
 
-use crate::{key_value::KeyValue, kv_scanner::KVScanner};
+use crate::{key_value::KeyValue, kv_scanner::KVScanner, Cell};
 
 type Segment = SkipSet<KeyValue>;
 
@@ -24,29 +24,75 @@ impl Memtable {
         self.snapshot = self.active.take();
         self.active = Some(Box::new(Segment::new()));
     }
-}
 
-impl KVScanner for Memtable {
-    fn scan(&self, start: Option<KeyValue>, end: Option<KeyValue>, read_point: Option<u64>) -> impl Iterator<Item = KeyValue> {
+    pub fn scan<'a>(&self, start: Option<KeyValue>, end: Option<KeyValue>, read_point: Option<u64>) -> Box<dyn Iterator<Item = KeyValue> + '_> {
         let segment = self.active.as_ref().unwrap();
+        
+        fn run_scan<'a, T: Iterator<Item = Entry<'a, KeyValue>>>(iter: T, read_point: Option<u64>) -> MemtableIterator<'a, T> {
+            MemtableIterator::new(iter, read_point)
+        }
 
         match (start, end) {
             (Some(start), None) => {
-                let v: Vec<KeyValue> = segment.range(start..).map(|x| { x.value().clone() }).collect();
-                v.into_iter()
+                Box::new(run_scan(segment.range(start..).into_iter(), read_point))
             },
             (None, None) => {
-                let v: Vec<KeyValue> = segment.range(..).map(|x| { x.value().clone() }).collect();
-                v.into_iter()
+                Box::new(run_scan(segment.range(..).into_iter(), read_point))
             }
             (None, Some(end)) => {
-                let v: Vec<KeyValue> = segment.range(..=end).map(|x| { x.value().clone() }).collect();
-                v.into_iter()
+                Box::new(run_scan(segment.range(..=end).into_iter(), read_point))
             }
             (Some(start), Some(end)) => {
-                let v: Vec<KeyValue> = segment.range(start..=end).map(|x| { x.value().clone() }).collect();
-                v.into_iter()
+                Box::new(run_scan(segment.range(start..=end).into_iter(), read_point))
             }
         }
     }
 }
+
+struct MemtableIterator<'a, T: Iterator<Item = Entry<'a, KeyValue>>> {
+    iter: T,
+    last_key: Option<Vec<u8>>,
+    read_point: Option<u64>
+}
+
+impl<'a, T: Iterator<Item = Entry<'a, KeyValue>>> MemtableIterator<'a, T> {
+    fn new(iter: T, read_point: Option<u64>) -> MemtableIterator<'a, T> {
+        MemtableIterator {
+            iter,
+            last_key: None,
+            read_point,
+        }
+    }
+}
+
+impl<'a, T: Iterator<Item = Entry<'a, KeyValue>>> Iterator for MemtableIterator<'a, T> {
+    type Item = KeyValue;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        while let Some(kv) = self.iter.next() {
+            let kv = kv.value();
+            // Check if MVCC enabled
+            if let Some(point) = self.read_point {
+                if kv.get_mvcc_id() > point {
+                    continue;
+                }
+            }
+
+            let key_vec = kv.get_key().to_vec();
+            if let Some(last_key) = &self.last_key {
+                if last_key.eq(&key_vec) {
+                    continue;
+                }
+            }
+
+            self.last_key = Some(key_vec);
+            return Some(kv.clone())
+        }
+
+        None
+    }
+}
+
+// impl KVScanner for Memtable {
+    
+// }
